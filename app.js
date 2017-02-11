796aa189f18da14d8f500ca290099ecdac30c301
Mason Ziedonis <Mason Ziedonis>
2017-02-10 20:06:39 -0800

    Finished initial info comment block

--- app.js
+++ app.js
@@ -4,8 +4,9 @@
     * Program Name: Clover Technical Challenge
     * Date Started: Feb. 10, 2017
     * Date Last Updated: Feb. 10, 2017
-    * 
+    * Amount of time spent coding: 2 hours
+    *
+    * Description: This is my file for the initial coding challenge portion of Clover's intership interview process. I have two hours to complete the challenge.
+    *
     * Copyright (C) 2017 Mason Ziedonis 
-
-    Extra.
 */
