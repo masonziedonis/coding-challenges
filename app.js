2c49f0cb500f608bbddd1b03ebfef8e01f475805
Mason Ziedonis <mziedonis@berkeley.edu>
2017-02-10 17:27:32 -0800

    Create app.js
    Starter skeleton with initial info comment block

---
+++ app.js
@@ -0,0 +1,9 @@
+/*!
+    * Author: Mason Ziedonis
+    * URL: www.masonziedonis.com
+    * Program Name: Clover Technical Challenge
+    * Date Started: Feb. 10, 2017
+    * Date Last Updated: Feb. 10, 2017
+    * 
+    * Copyright (C) 2017 Mason Ziedonis 
+*/
